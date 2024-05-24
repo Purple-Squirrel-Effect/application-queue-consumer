01375217bdd458577a7f0fd85d19d964f121761d
Pieter den Toom <pieter@experiencerepublic.nl>
2024-05-24 13:53:27 +0200

    Fetch company email address from KV

--- src/index.js
+++ src/index.js
@@ -18,28 +18,37 @@ export default {
 		for (const message of batch.messages) {
 			const email = message.body.email;
 			const application = message.body;
+			// Turn the companyName to lowercase.
+			const companyName = application.companyName.toLowerCase();
 
-			// Initiate the resend sender using the environment variable RESEND_API_KEY
-			const resend = new Resend(env.RESEND_API_KEY);
+			// Try to fetch the value from the KV CompaniesKV, using the companyName as key.
+			const companyValue = await env.CompaniesKV.get(companyName);
+			const companyEmailAddress = JSON.parse(companyValue).email;
 
-			try {
-				const sentEmail = await resend.emails.send({
-					from: 'Genius <noreply@wearegenius.nl>',
-					to: ['pieter@experiencerepublic.nl'],
-					subject: 'Nieuwe sollicitatie',
-					html: `
+			// If there is an emailaddress on record, try to send the email.
+			// Test what happens if there is no emailaddress on record.
+			if (companyEmailAddress) {
+				try {
+					const resend = new Resend(env.RESEND_API_KEY);
+					const sentEmail = await resend.emails.send({
+						from: 'Genius <noreply@wearegenius.nl>',
+						to: [companyEmailAddress],
+						subject: 'Nieuwe sollicitatie',
+						html: `
 						<p>Er is een nieuwe sollicitatie binnengekomen!</p>
 						<p>Naam: ${application.name}</p>
 						<p>Email: ${application.email}</p>
 						<p>Telefoonnummer: ${application.phone}</p>
 					`,
-				});
-			} catch (error) {
-				console.error('Error processing message:', error);
+					});
+					console.log(sentEmail);
+				} catch (error) {
+					console.error('Error processing message:', error);
+				}
 			}
 
 			try {
-				const value = await env.KV.get(email);
+				const value = await env.ApplicationsKV.get(email);
 				let existingApplications;
 				if (value) {
 					existingApplications = JSON.parse(value);
@@ -47,10 +56,10 @@ export default {
 					existingApplications = [];
 				}
 				existingApplications.push(application);
-				await env.KV.put(email, JSON.stringify(existingApplications));
+				await env.ApplicationsKV.put(email, JSON.stringify(existingApplications));
 			} catch (error) {
 				console.error('Error processing message:', error);
-				// Re-queue or send to failure queue based on your chosen approach
+				// TODO: Re-queue or send to failure queue based on your chosen approach
 				return Promise.reject(error);
 			}
 		}
